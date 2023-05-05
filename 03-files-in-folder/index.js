59120ef0d3969ddbab19b9e4950a15ef7290061a
Ruslan <russl.murzin@gmail.com>
2023-05-06 01:21:08 +0300

    feat: solve task 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const {readdir} = require('fs/promises');
+const {stat} = require('fs');
+const path = require('path');
+
+readdir(
+  path.join(__dirname, 'secret-folder'),
+  {withFileTypes:true}
+)
+.then(data => {
+  for(let file of data) {
+    if(file.isFile()) {
+      const filePath = path.join(__dirname, 'secret-folder', `${file.name}`)
+      stat(
+        filePath,
+        (err, stats) => {
+          if (err) throw err
+          const fileName = path.basename(filePath, path.extname(filePath))
+          const fileExtension = `${file.name}`.split('.').at(-1)
+          const fileSize = `${stats.size/1024}kb`
+          console.log(`${fileName} - ${fileExtension} - ${fileSize}`)
+        }
+      )
+    }
+  }
+})
