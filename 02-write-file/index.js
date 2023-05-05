4704c1655d25b065893402816a0a5ae844d473c7
Ruslan <russl.murzin@gmail.com>
2023-05-05 19:18:01 +0300

    feat: solve task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout, exit} = require('process');
+const readline = require('readline');
+const rl = readline.createInterface(stdin);
+
+process.on('exit', () => {
+  stdout.write('Досвидания!');
+});
+
+process.on('SIGINT', () => {
+  exit();
+});
+
+fs.writeFile(
+  path.join(__dirname, 'file.txt'),
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+console.log('Приветствую! Введите ваш текст:');
+
+rl.on('line', (input) => {
+  if(input === 'exit') exit();
+  fs.appendFile(
+    path.join(__dirname, 'file.txt'),
+    `${input}\n`,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+});
