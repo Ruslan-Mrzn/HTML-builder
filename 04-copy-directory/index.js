7b34bb84f83f30af0430d6dc6804c2c18801f271
Ruslan <russl.murzin@gmail.com>
2023-05-06 19:56:56 +0300

    feat and refactor: solve task 05 and update js-files according eslint-rules

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,37 +1,37 @@
 const {mkdir,readdir} = require('fs/promises');
-const {copyFile} = require('fs')
+const {copyFile} = require('fs');
 const path = require('path');
 
-const newPath = path.join(__dirname, 'files-copy')
-const oldPath = path.join(__dirname, 'files')
+const newPath = path.join(__dirname, 'files-copy');
+const oldPath = path.join(__dirname, 'files');
 
 const copyDir = () => {
   mkdir(
     newPath,
     {recursive: true},
     (err) => {
-      if (err) throw err
+      if (err) throw err;
     }
   )
-  .then(() => {
-    readdir(
-      oldPath
-    )
-    .then((files) => {
-      for(let file of files) {
-        copyFile(
-          path.join(oldPath, file),
-          path.join(newPath, file),
-          (err) => {
-            if (err) throw err
+    .then(() => {
+      readdir(
+        oldPath
+      )
+        .then((files) => {
+          for(let file of files) {
+            copyFile(
+              path.join(oldPath, file),
+              path.join(newPath, file),
+              (err) => {
+                if (err) throw err;
+              }
+            );
           }
-        )
-      }
+        });
     })
-  })
-  .catch(err => {
-    if (err) throw err
-  })
-}
+    .catch(err => {
+      if (err) throw err;
+    });
+};
 
 copyDir();
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const {readdir} = require('fs/promises');
+
+const cssBundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const stylesPath = path.join(__dirname, 'styles');
+const readAndWriteFile = (filePath) => {
+  const readableStream = fs.createReadStream(filePath, 'utf-8');
+  readableStream.on(
+    'data',
+    chunk => fs.appendFile(
+      cssBundlePath,
+      chunk,
+      (err) => {
+        if (err) throw err;
+      }
+    )
+  );
+};
+
+fs.writeFile(
+  cssBundlePath,
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+readdir(
+  stylesPath,
+  {withFileTypes:true}
+)
+  .then(data => {
+    for(let file of data) {
+      if(file.isFile() && file.name.endsWith('.css')) {
+        const filePath = path.join(stylesPath, `${file.name}`);
+        readAndWriteFile(filePath);
+      }
+    }
+  });
+
+
