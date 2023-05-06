3f92ec2a77a4448f90dbc8e0dfc58cbfc1f0ac3e
Ruslan <russl.murzin@gmail.com>
2023-05-06 10:59:34 +0300

    feat: solve task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const {mkdir,readdir} = require('fs/promises');
+const {copyFile} = require('fs')
+const path = require('path');
 
+const newPath = path.join(__dirname, 'files-copy')
+const oldPath = path.join(__dirname, 'files')
+
+mkdir(
+  newPath,
+  {recursive: true},
+  (err) => {
+    if (err) throw err
+  }
+)
+.then(() => {
+  readdir(
+    oldPath
+  )
+  .then((files) => {
+    for(let file of files) {
+      copyFile(
+        path.join(oldPath, file),
+        path.join(newPath, file),
+        (err) => {
+          if (err) throw err
+        }
+      )
+    }
+  })
+})
+.catch(err => {
+  if (err) throw err
+})
