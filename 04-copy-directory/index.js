68711260187bda568e61668a3817bcfbb285b528
Ruslan <russl.murzin@gmail.com>
2023-05-06 13:29:24 +0300

    refactor: add function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,29 +5,33 @@ const path = require('path');
 const newPath = path.join(__dirname, 'files-copy')
 const oldPath = path.join(__dirname, 'files')
 
-mkdir(
-  newPath,
-  {recursive: true},
-  (err) => {
-    if (err) throw err
-  }
-)
-.then(() => {
-  readdir(
-    oldPath
-  )
-  .then((files) => {
-    for(let file of files) {
-      copyFile(
-        path.join(oldPath, file),
-        path.join(newPath, file),
-        (err) => {
-          if (err) throw err
-        }
-      )
+const copyDir = () => {
+  mkdir(
+    newPath,
+    {recursive: true},
+    (err) => {
+      if (err) throw err
     }
+  )
+  .then(() => {
+    readdir(
+      oldPath
+    )
+    .then((files) => {
+      for(let file of files) {
+        copyFile(
+          path.join(oldPath, file),
+          path.join(newPath, file),
+          (err) => {
+            if (err) throw err
+          }
+        )
+      }
+    })
   })
-})
-.catch(err => {
-  if (err) throw err
-})
+  .catch(err => {
+    if (err) throw err
+  })
+}
+
+copyDir();
